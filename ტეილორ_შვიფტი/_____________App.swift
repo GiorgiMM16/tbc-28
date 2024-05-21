97edaef42be323a6a3b0e57d7b4fcd4fd8a6c13f
GiorgiMM16 <michitashvili.giorgi3@gmail.com>
2024-05-22 00:26:48 +0400

    Initial Commit

---
+++ ტეილორ_შვიფტი/_____________App.swift
@@ -0,0 +1,17 @@
+//
+//  _____________App.swift
+//  ტეილორ_შვიფტი
+//
+//  Created by Giorgi Michitashvili on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct _____________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
